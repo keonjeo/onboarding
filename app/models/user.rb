82ad11b41176664d19083014f6fa11b034334195
Keon Ye <keon@shoplineapp.com>
2019-03-26 16:57:37 +0800

    use the mongodb gem instead of the sqlite gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
-class User < ActiveRecord::Base
+class User
+  include Mongoid::Document
+
+  field :first_name, type: String
+  field :last_name, type: String
+  field :age, type: Integer
+  field :gender, type: String
 
   validates_presence_of :first_name, :last_name
-  validates_numericality_of :age, greater_than: 0
+  validates_numericality_of :age, greater_than: 0, only_integer: true
   validates :gender, inclusion: { in: %w(male female others) }
 
 end
